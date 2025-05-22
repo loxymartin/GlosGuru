3ddb7102f3c2672476df99deb4d4b68a37e1b024
Martin Lund <fippel@gmail.com>
2025-05-22 22:01:36 +0200

    WIP WordListEdit component

--- GlosGuru.Web/Model/WordList.cs
+++ GlosGuru.Web/Model/WordList.cs
@@ -1,7 +1,10 @@
 ﻿namespace GlosGuru.Web.Model;
 
+using System.ComponentModel.DataAnnotations;
+
 public class WordList
 {
+    [Key]
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
 
